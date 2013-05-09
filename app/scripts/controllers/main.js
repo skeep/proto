e3dc29172a853ffde1572f610f29c0fc3722de37
Pinakin Mistry <pinakin.nmistry@gmail.com>
2013-05-09 18:35:01 +0530

    Save file to disk functional

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,6 +19,7 @@ angular.module('protoApp').controller('MainCtrl', function ($scope) {
 			$scope.screen = $scope.screens[$scope.screenList[0].id];
 			$scope.$apply();
 		}
+		addSavetoFileButton($scope);
 	}
 
 	$scope.spotId = null;
@@ -281,3 +282,20 @@ function errorHandler(e) {
   console.log('Error: ' + msg);
 }
 
+function addSavetoFileButton(scope) {
+	Downloadify.create('downloadFile',{
+		filename: function(){
+			return 'appdata.txt';
+		},
+		data: JSON.stringify(scope.screens),
+		onComplete: function(){ alert('Your File Has Been Saved!'); },
+		onCancel: function(){ alert('You have cancelled the saving of this file.'); },
+		onError: function(){ alert('You must put something in the File Contents or there will be nothing to save!'); },
+		swf: 'media/downloadify.swf',
+		downloadImage: 'images/download.png',
+		width: 100,
+		height: 30,
+		transparent: true,
+		append: false
+	});
+}
